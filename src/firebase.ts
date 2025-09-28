0a3ec0c37f20190290466b29f1beb6195e0a4f85
SujithMMaddin <sujithmmaddin31@gmail.com>
2025-09-28 23:23:17 +0530

    Initial commit

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxczHU-2IrwXm7vADULDBb1gHA_V4SFhY",
-  authDomain: "farmdirect-7c6f8.firebaseapp.com",
-  projectId: "farmdirect-7c6f8",
-  storageBucket: "farmdirect-7c6f8.firebasestorage.app",
-  messagingSenderId: "725451486867",
-  appId: "1:725451486867:web:83ebf4289e87384193ff66",
-  measurementId: "G-GYYKH2PLZN",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
