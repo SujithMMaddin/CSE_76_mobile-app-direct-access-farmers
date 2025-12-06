46c2a02787fa9fc6936d5d1fa0a8f74c4a61ad0b
Sujith M Maddin <sujithmmaddin31@gmail.com>
2025-12-06 12:13:25 +0530

    Delete migrations directory

--- migrations/2.sql
+++
@@ -1,3 +0,0 @@
-
--- Make the first user (app owner) an admin automatically
-UPDATE user_profiles SET role = 'admin' WHERE id = (SELECT MIN(id) FROM user_profiles);
--- migrations/2/down.sql
+++
@@ -1,4 +0,0 @@
-
--- Revert the first user back to their original role (this is a one-way change for security)
--- Admin privileges should not be automatically removed
-SELECT 1;
